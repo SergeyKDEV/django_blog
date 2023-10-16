2d9ff11ba818532ac087f6ea39c11be99d6f4af3
Sergey Kulbida <kulbida.sergey@outlook.com>
2023-10-16 18:14:42 +0700

    code according to PEP8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from core.models import PublishedModel
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django_cleanup import cleanup
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -21,14 +21,6 @@ urlpatterns = [
          ),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [path('__debug__/',
-                    include("debug_toolbar.urls")
-                         ),
-                    ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'pages.views.page_not_found'
